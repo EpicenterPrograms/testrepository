5fadc89ccc203ff0e1fcc2a04135a0053e8c0cbd
Robert <ambitiousbusinessowner@gmail.com>
2018-08-04 19:04:07 -0500

    prevented automation in the web worker

--- webworker.js
+++ webworker.js
@@ -1,3 +1,4 @@
+var Standards = { general: { options: { automation: "none" } } };
 importScripts("https://epicenterprograms.github.io/standards/behavior/general.js");
 
 self.addEventListener("message", function (message) {
