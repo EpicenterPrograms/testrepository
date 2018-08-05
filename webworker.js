e09d56cdb7a9831ba7c692a020b78856d98d10e6
Robert <ambitiousbusinessowner@gmail.com>
2018-08-04 19:30:26 -0500

    tested web worker timing

--- webworker.js
+++ webworker.js
@@ -2,5 +2,21 @@ var Standards = { general: { options: { automation: "none" } } };
 importScripts("https://epicenterprograms.github.io/standards/behavior/general.js");
 
 self.addEventListener("message", function (message) {
-	self.postMessage(message.data);
+	self.postMessage(0);
+	setTimeout(function () {
+		self.postMessage(1);
+	}, 1000);
+	setTimeout(function () {
+		self.postMessage(2);
+	}, 2000);
+	setTimeout(function () {
+		self.postMessage(3);
+	}, 3000);
+	setTimeout(function () {
+		self.postMessage(4);
+	}, 4000);
+	setTimeout(function () {
+		self.postMessage(message.data);
+		self.close();  // closes the web worker
+	}, 5000);
 });
